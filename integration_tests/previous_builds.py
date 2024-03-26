1e15852eb8f9408a41baa6e4d3b90e882c6047ce
Battula-anil-app-could <124858298+Battula-anil-app-could@users.noreply.github.com>
2024-03-26 12:38:17 +0530

    v0.0.3

--- integration_tests/previous_builds.py
+++ integration_tests/previous_builds.py
@@ -22,25 +22,57 @@ class PreviousBuild:
 previous_builds: List[PreviousBuild] = [
     PreviousBuild(
         name="a.1",
-        project_archive_url="https://github.com/Dharitri-org/drt-reproducible-contract-build-example-sc/archive/refs/tags/v0.3.0.zip",
-        project_relative_path_in_archive=None,
+        project_archive_url="https://github.com/Dharitri-org/mx-reproducible-contract-build-example-sc/archive/refs/tags/v0.4.0.zip",
+        project_relative_path_in_archive="mx-reproducible-contract-build-example-sc-0.4.0",
         packaged_src_url=None,
         contract_name=None,
         expected_code_hashes={
-            "adder": "9fd12f88f9474ba115fb75e9d18a8fdbc4f42147de005445048442d49c3aa725"
+            "adder": "9fd12f88f9474ba115fb75e9d18a8fdbc4f42147de005445048442d49c3aa725",
+            "multisig": "2101bc2a7a31ea42e5ffaadd86c1640009690e93b1cb46c3566ba5eac2984e36",
+            "multisig-full": "ef468403354b6d3a728f86101354359fe6864187d216f674d99b31fc05313a39",
+            "multisig-view": "3690af76be10c0520e3c3545cde8d9ef6a15c2d0af74dbd8704b4909644049c9"
+        },
+        docker_image="dharitriorg/sdk-rust-contract-builder:v5.1.0"
+    ),
+    PreviousBuild(
+        name="a.2",
+        project_archive_url="https://github.com/Dharitri-org/mx-reproducible-contract-build-example-sc/archive/refs/tags/v0.4.3.zip",
+        project_relative_path_in_archive="mx-reproducible-contract-build-example-sc-0.4.3",
+        packaged_src_url=None,
+        contract_name=None,
+        expected_code_hashes={
+            "adder": "9fd12f88f9474ba115fb75e9d18a8fdbc4f42147de005445048442d49c3aa725",
+            "multisig": "b73050629c11b1f1a20ca6232abcef07897624195691552e3f2e2fce47822166",
+            "multisig-full": "37c3b90bdaa7d8d203385c91b0b5cb4d3c444ab9ec5263351978046a545854e3",
+            "multisig-view": "ebaf987b041fcda297da71291d76736e4e98a1e449e5ec37908cdc0198e8be37"
+        },
+        docker_image="dharitriorg/sdk-rust-contract-builder:v5.3.0"
+    ),
+    PreviousBuild(
+        name="a.3",
+        project_archive_url="https://github.com/Dharitri-org/mx-exchange-sc/archive/refs/heads/reproducible-v2.4.1-pair-safe-price-v2.zip",
+        project_relative_path_in_archive="mx-exchange-sc-reproducible-v2.4.1-pair-safe-price-v2",
+        packaged_src_url=None,
+        contract_name=None,
+        expected_code_hashes={
+            "pair": "e9f117971963cb3c24b14e2a7698d48c170335af2f5c8167774c48c3c1c654e3",
+            "pair-full": "f1af2b2bb42a9f035745777e4b2d4f72478569224f204d0d0103801faff9663a",
+            "safe-price-view": "b5a657445ae74423c60210c88a6fa89b0bd4bdd00d5f06e788e14495bccc34c9"
         },
         docker_image="sdk-rust-contract-builder:next"
     ),
     PreviousBuild(
-        name="a.2",
-        project_archive_url="https://github.com/Dharitri-org/drt-reproducible-contract-build-example-sc/archive/refs/heads/add-multisig.zip",
-        project_relative_path_in_archive=None,
+        name="a.4",
+        project_archive_url="https://github.com/Dharitri-org/mx-exchange-sc/archive/refs/heads/reproducible-v2.5.2-governance-merkle-tree.zip",
+        project_relative_path_in_archive="mx-exchange-sc-reproducible-v2.5.2-governance-merkle-tree",
         packaged_src_url=None,
         contract_name=None,
         expected_code_hashes={
-            "multisig": "5cc02a06d3c921a31ecbfe351a69ac2309c7c42a4a82152ee0df09cf8c4c73b6",
-            "multisig-full": "6a739f31c52fd2284ff77a8ef6f5c50f94ab8511fe1f772e5400d3c0e425a46b",
-            "multisig-view": "50da33a67b84a8b754ac8efdb30cb9424ff2173622a6d6083522c21f36bde8ac"
+            "lkmex-transfer": "49809df9f07839f965f8197721083cf403c3db969ddc47b5940b4ee8b464af92",
+            "router": "3257d57945736298c96aa23f99ea0fba3b6da01f9d2103d81230d05cff62cb5a",
+            "pair": "e9f117971963cb3c24b14e2a7698d48c170335af2f5c8167774c48c3c1c654e3",
+            "locked-token-wrapper": "1d317cfa2bbe22ea1f878f8a32f90712d49f68a01665b86657d36910b430522f",
+            "safe-price-view": "b5a657445ae74423c60210c88a6fa89b0bd4bdd00d5f06e788e14495bccc34c9"
         },
         docker_image="sdk-rust-contract-builder:next"
     )
